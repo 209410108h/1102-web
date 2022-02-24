9c7e74681fcfc2f315f3dd39caa023858ebbe4aa
209410108h <hank5947887@gmail.com>
2022-02-24 21:11:35 +0800

    first commit

---
+++ src/App_08.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ import T6_08 from './tutorial/6-useReducer/T6_08_index2'
+
+//import Grocery_xx from './demo/grocery/Grocery_xx';
+
+const App_08 = () => {
+
+  return (
+     //<Grocery_xx />
+
+     <div className='container'>
+       <h2>Tutorial 6 (V2) - 209410108</h2>
+       <T6_08 />
+     </div>
+  );
+};
+
+export default App_08;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_08 from './App_08';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App_08 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
